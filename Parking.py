dddc0fcbd40db3bd12cc5ec6cf9245a04e5c4492
zbw <langyuefanxing@gmail.com>
2016-05-03 09:52:24 +0800

    change to python

--- Parking.py
+++ Parking.py
@@ -1,17 +1,38 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
-@app.route('/index')
-def index():
-    pass
+@app.route('/login')
+def login():
+    return render_template('home00.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('home01.html')
+
+
+@app.route('/reserve')
+def reserve():
+    return render_template('reserve.html')
+
+
+@app.route('/pad1')
+def pad1():
+    return render_template('pad1.html')
+
+
+@app.route('/pad2')
+def pad2():
+    return render_template('pad2.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
