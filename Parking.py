5e7c0ccf2043061b48c4e64e928834fd040a26d4
Glenzh <zhang9559@gmail.com>
2016-05-03 12:59:42 +0800

    add login register

--- Parking.py
+++ Parking.py
@@ -1,16 +1,64 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import Util
 
 app = Flask(__name__)
 
 
+@app.before_request
+def before_request():
+    """  global status  name cookie bookinglist"""
+    pass
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    result = Util.user_register(
+        request.form["registUsername"],
+        request.form["registEmail"],
+        "110120",
+        request.form["registPassword"])
+    if result == "success":
+        return render_template('home01.html')
+    else:
+        return render_template('index.html')
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    """  check ording"""
+    email = request.form.get('inputEmail')
+    password = request.form.get('inputPassword')
+    result = Util.user_login(email, password)
+    if result == "success":
+        return render_template('home00.html')
+    else:
+        return render_template('index.html')
+
+
+@app.route('/reserve')
+def reserve():
+    """ information about book"""
+    return render_template('reserve.html')
+
+
+@app.route('/pad1')
+def pad1():
+    return render_template('pad1.html')
 
 
 @app.route('/index')
 def index():
-    pass
+    return render_template('index.html')
+
+
+@app.route('/pad2')
+def pad2():
+    return render_template('pad2.html')
 
 
 if __name__ == '__main__':
---
+++ Util.py
@@ -0,0 +1,72 @@
+import MySQLdb
+
+
+def get_conn():
+    # return MySQLdb.connect(user="root", passwd="root", db="parkingLot",
+    # unix_socket="/opt/lampp/var/mysql/mysql.sock")
+    return MySQLdb.connect(host="121.42.43.36", user="root", passwd="root",
+                           db="parkinglot", port=3306)
+
+
+def user_exist(cur="", name="", password=""):
+    if cur != "":
+        cur.execute("SELECT * FROM `user` WHERE  `Name`='%s'" % (name))
+        return len(cur.fetchall())
+    else:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM `user` WHERE `Email`='%s' AND PassWord='%s'"
+                    % (name, password))
+        result = len(cur.fetchall())
+        conn.close()
+        return result
+
+
+def user_login(Name, Password):
+    if user_exist(name=Name, password=Password) == 0:
+        return "fail"
+    else:
+        return "success"
+
+
+def user_register(name, email, phonenumber, password):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    if user_exist(cur, name) == 0:
+        try:
+            cur.execute("INSERT INTO `user`( `Name`, `Email`, `PhoneNumber`, \
+                    `PassWord` ) VALUES ('%s','%s','%s','%s')" %
+                        (name, email, phonenumber, password))
+            conn.commit()
+            conn.close()
+            return "success"
+        except:
+            conn.rollback()
+            conn.close()
+            return "fail"
+    else:
+        conn.close()
+        return "exist"
+
+
+class Booking(object):
+
+    """Docstring for Booking. """
+
+    def __init__(self, id="", name="", beginTime="", endTime="", carNumber=""):
+        """TODO: to be defined1. """
+        self.id = id
+        self.name = name
+        self.beginTime = beginTime
+        self.endTime = endTime
+        self.carNumber = carNumber
+
+    def book(self):
+        pass
+
+    def alter_book(self):
+        pass
+
+    def cancel_book(self):
+        pass
