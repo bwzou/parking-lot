1260edb424ab6cc24dae5e77c93eaeb438b84180
zbw <langyuefanxing@gmail.com>
2016-05-03 15:06:13 +0800

    Merge branch 'master' into zb
    # Conflicts:
    #	Parking.py
    #	templates/index.html

--- Parking.py
+++ Parking.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, render_template
+# --coding:utf8--
+from flask import Flask, request, render_template, session, redirect
 import Util
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
-@app.before_request
-def before_request():
-    """  global status  name cookie bookinglist"""
-    pass
-
-
 @app.route('/')
 def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/index')
+def index():
     return render_template('index.html')
 
 
@@ -35,6 +35,7 @@ def login():
     password = request.form.get('inputPassword')
     result = Util.user_login(email, password)
     if result == "success":
+        session['username'] = email                     # 添加到session
         return render_template('home00.html')
     else:
         return render_template('index.html')
@@ -46,38 +47,9 @@ def reserve():
     return render_template('reserve.html')
 
 
-@app.route('/pad1')
-def pad1():
-    return render_template('pad1.html')
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/pad2')
-def pad2():
-    return render_template('pad2.html')
-
-"""
-@app.route('/index')
-def index():
-    if 'username' in session:
-        return 'Logged in as %s' % session.get('username')  # 应该调转到相应的界面去
-
-    return render_template('index.html')
-
-
-@app.route('/login')
-def login():
-    session['username'] = 123         # get data from form
-    return render_template('home00.html')
-
-
-@app.route('/register')      # 注册完之后重新回到主页
-def register():
-    return render_template('home01.html')
+@app.route('/customer_index')
+def customer_index():
+    pass
 
 
 @app.route('/logout')
@@ -86,20 +58,9 @@ def logout():
     redirect('/')
 
 
-@app.route('/reserve')
-def reserve():
-    return render_template('reserve.html')
-
-
-@app.route('/pad1')
-def select_by_custom():
-    return render_template('pad1.html')
-
-
 @app.errorhandler(404)             # 扑捉错误并作出响应
 def page_not_found(error):
     return render_template('page_not_found.html'), 404   # 告诉 Flask，该页的错误代码是 404 ，即没有找到。默认为 200
-"""
 
 
 if __name__ == '__main__':
