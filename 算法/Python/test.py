1efd6132f49993bce47b9c9aa498f8ab1489b39f
zuodexin <zuodexin@mail.nankai.edu.cn>
2016-05-04 13:37:07 +0800

    添加动态加载算法示例

--- 算法/Python/test.py
+++ 算法/Python/test.py
@@ -5,4 +5,7 @@ from Parking import insert
 # param4:length of the new reservation.Notice endtime=start time+ length -1
 # return [] when too crowed to insert
 # return [(rid,-1,pid),(rid,from,to),...] when success.
+import sys
+sys.path.append(".\build\lib.win-amd64-2.7")
+import Parking
 print insert([(1,1,1,3),(2,1,6,3),(3,2,2,5),(4,3,1,4),(5,3,7,2)],3,4,3)
