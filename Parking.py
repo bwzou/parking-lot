2d20e7d47a9d319bbf893dc784878d5052eee05d
zbw <langyuefanxing@gmail.com>
2016-05-10 23:27:50 +0800

    修改了查询车位时的一些bug

--- Parking.py
+++ Parking.py
@@ -204,24 +204,29 @@ def lot():
 
 @app.route('/getlotname', methods=["POST", "GET"])
 def getlotname():
-    print "nihao"
     if request.method == "POST":
-        print "nibuhao"
         order_number = request.form['inputNumber']
         plate_number = request.form['inputLicenseNumber']
         print order_number
         print plate_number
         if order_number == "" and plate_number == "":
-            redirect('/lot')
+            flash(u'you must input either of blanks', 'error')  # 消息错误提示)
+            return render_template('pad1.html')
         else:
             if order_number != "":
                 result = Util.Booking.query_book(order_number)
                 if result:
                     ans = Util.ParkingLot.set_lot_status(result.PID)
+                else:
+                    flash(u'The order number is not existing,please try again ', 'error')  # 消息错误提示)
+                    return render_template('pad1.html')
             else:
                 result = Util.Booking.query_book_by_plate(plate_number)      # plate_number不是唯一，这里要修正
                 if result:
                     ans = Util.ParkingLot.set_lot_status(result.PID)
+                else:
+                    flash(u'The plate number is not existing,please try again ', 'error')  # 消息错误提示)
+                    return render_template('pad1.html')
         return render_template('pad2.html', result=result)
     return render_template('pad2.html', result=None)
 
